2a838543582f3c510ce935eefbece96a856de3a9
Sakumzi <skenjanakeith@gmail.com>
2022-12-26 07:34:38 -0800

    All tasks

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if my_list and len(my_list):
+        numm = 0
+        denim = 0
+        for tup in my_list:
+            num += (tup[0] * tup[1])
+            denom += (tup[1])
+            return (num/denom)
+        return 0
