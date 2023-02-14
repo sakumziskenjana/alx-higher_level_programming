de062004b0b19153f9ca6026012cc416b1aea4d1
Sakumzi <skenjanakeith@gmail.com>
2023-02-14 10:43:44 -0800

    Tasks

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,4 +1,5 @@
 -- create table first_table in MYSQL server
 -- passed as argument of mysql command
 -- does not fail
-CREATE DATABASE first_table IF NOT EXISTS
+CREATE DATABASE first_table IF NOT EXISTS (id INT,
+name VARCHAR(256))
