97ae4b098ab2ea2fbb42c2b12e0ca7c7ac097331
Sakumzi <skenjanakeith@gmail.com>
2023-02-14 09:52:58 -0800

    Tasks

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,3 +1,3 @@
--- delete the database hbtn_0c_0 in MYSQL server
--- does not fail if hbtn_0c_0 does not exist
-DELETE DATABASE IF EXISTS hbtn_0c_0
+-- remove the database hbtn_0c_0 in MYSQL server
+-- does not fail
+DROP DATABASE IF EXISTS hbtn_0c_0
